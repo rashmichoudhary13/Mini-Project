624e766f4976dd9dd7a2714f76a898a096aa5e96
TechAryan24 <bhekarearyan@gmail.com>
2023-10-28 19:04:43 +0530

    removed dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,3 +40,13 @@ $('.counting').each(function () {
 
 });
 
+     $(document).ready(function () {
+      // Show Register Modal when Register button is clicked
+      $('.button-01 a').on('click', function (e) {
+        e.preventDefault();
+        $('#registerModal .modal-content').load('form.html', function () {
+          $('#registerModal').modal('show');
+        });
+      });
+    });
+  
