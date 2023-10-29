3c52d88a00aeeda3a97520cbdf3e51e094cf9e3c
rashmichoudhary13 <rashmichoudhary019@gmail.com>
2023-10-29 15:11:53 +0530

    Dleted bootstrap.min.css  from form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,14 @@
 $(document).ready(function () {
-  $('.menu-toggle').click(function () {
-    $('.menu-toggle').toggleClass('active')
-    $('.menu').toggleClass('active')
+  // Show Register Modal when Register button is clicked
+  $('.button-01 a').on('click', function (e) {
+    e.preventDefault();
+    $('#registerModal .modal-content').load('form.html', function () {
+      $('#registerModal').modal('show');
+    });
   });
 });
 
-$(() => {
-
-  //On Scroll Functionality
-  $(window).scroll(() => {
-    var windowTop = $(window).scrollTop();
-    windowTop > 50 ? $('header').addClass('og-hf') : $('header').removeClass('og-hf');
-  });
+// Close modal when close button is clickeds
+$('#registerModal .close').on('click', function () {
+  $('#registerModal').modal('hide');
 });
-
-$('.counting').each(function () {
-  var $this = $(this),
-    countTo = $this.attr('data-count');
-
-  $({
-    countNum: $this.text()
-  }).animate({
-      countNum: countTo
-    },
-
-    {
-
-      duration: 3000,
-      easing: 'linear',
-      step: function () {
-        $this.text(Math.floor(this.countNum));
-      },
-      complete: function () {
-        $this.text(this.countNum);
-        //alert('finished');
-      }
-
-    });
-
-});
-
-     $(document).ready(function () {
-      // Show Register Modal when Register button is clicked
-      $('.button-01 a').on('click', function (e) {
-        e.preventDefault();
-        $('#registerModal .modal-content').load('form.html', function () {
-          $('#registerModal').modal('show');
-        });
-      });
-    });
-  
