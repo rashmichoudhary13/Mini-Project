d7ccc885a2f2a757c39c07dccf833ff29e29cdd8
rashmichoudhary13 <rashmichoudhary019@gmail.com>
2023-10-30 00:57:53 +0530

    Added dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+// Dropdown toggling
+// Select all elements with the class "select-menu"
+const optionMenus = document.querySelectorAll(".select-menu");
+
+optionMenus.forEach((optionMenu) => {
+  const selectBtn = optionMenu.querySelector(".select-btn");
+  const options = optionMenu.querySelectorAll(".option");
+  const sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+  selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      let selectedOption = option.querySelector(".option-text").innerText;
+      sBtn_text.innerText = selectedOption;
+      optionMenu.classList.remove("active");
+    });
+  });
+});
+
+
+
+
+
 $(document).ready(function () {
   // Show Register Modal when Register button is clicked
   $('.button-01 a').on('click', function (e) {
@@ -12,3 +36,9 @@ $(document).ready(function () {
 $('#registerModal .close').on('click', function () {
   $('#registerModal').modal('hide');
 });
+
+
+
+
+
+
